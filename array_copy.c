c17915780260e3023b579190093ab7c0f20afa74
itspratik35 <116029297+itspratik35@users.noreply.github.com>
2022-10-31 17:24:28 +0530

    Create array_copy.c
    Copy one array from another in C.

---
+++ array_copy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()    
+{        
+    int a1[] = {1, 2, 3, 4, 5};   
+    int length = sizeof(a1)/sizeof(a1[0]);
+    int a2[length];    
+    for (int i = 0; i < length; i++) {     
+        a2[i] = a1[i];     
+    }    
+    printf("Elements of original array: \n");    
+    for (int i = 0; i < length; i++) {     
+        printf("%d ", a1[i]);    
+    }   
+    printf("\n");    
+    printf("Elements of new array: \n");    
+    for (int i = 0; i < length; i++) {     
+        printf("%d ", a2[i]);    
+    }
+    return 0;    
+}
